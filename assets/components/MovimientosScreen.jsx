1078f9934be43c432b2096970125ece1bf3d3bef
hervis30 <hervisjonat@gmail.com>
2022-10-18 21:52:49 -0500

    se actualiza vista movimientos

--- assets/components/MovimientosScreen.jsx
+++ assets/components/MovimientosScreen.jsx
@@ -1,29 +1,9 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text } from 'react-native';
 import { styles } from '../Styles/Estilos';
 
-const MovimientosScreen = ({ setActivo }) => {
+const MovimientosScreen = () => {
     return (
         <View style={styles.container}>
-            <TouchableOpacity
-                style={{ backgroundColor: 'blue', padding: 10, borderRadius: 10 }}
-                onPress={() => {
-                    setActivo(true)
-                    console.log("pasando a true")
-
-                }}
-            >
-                <Text style={{ color: 'white', fontSize: 40 }}>true</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                style={{ backgroundColor: 'blue', padding: 10, borderRadius: 10 }}
-                onPress={() => {
-                    setActivo(false)
-                    console.log("pasando a false")
-                }}
-            >
-                <Text style={{ color: 'white', fontSize: 40 }}>false</Text>
-            </TouchableOpacity>
             <Text>Bienvenido(a) a la la sesion de Movimientos </Text>
             <Text>La sesion estara disponible proximante</Text>
         </View>
